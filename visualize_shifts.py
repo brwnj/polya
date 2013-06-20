ca59672884259ca0459bf9c0f1cbf814dca4a1b1
brwnj <brwnjm@gmail.com>
2013-06-20 12:18:07 -0600

    process multiple comparisons in single shifts file; write gzipped track for each comparison

--- visualize_shifts.py
+++ visualize_shifts.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
-From the shifts and merged sites, create a bed12 in order to improve shift
-visualization in UCSC browser.
+From the shifts and merged sites, create a bed12 for each comparison with a
+trackline to visualize shift in UCSC browser. Strand is determined from the file
+name and must be present.
 """
 import sys
+import gzip
+import operator
 from toolshed import reader
+
+class StrandNotFound(Exception):
+    pass
 
 class Bed(object):
     def __init__(self, toks):
@@ -13,7 +19,14 @@ class Bed(object):
         self.start, self.stop = map(int, toks[1:3])
         self.name, self.score, self.strand = toks[3:6]
 
-def bed12line(chr, start, stop, strand, shift):
+def get_strand(fname):
+    if "pos" in fname:
+        return "pos"
+    if "neg" in fname:
+        return "neg"
+    raise StrandNotFound()
+
+def bed12line(chrom, start, stop, strand, shift):
     if strand == "+":
         arrow = "-" if shift == "proximal" else "+"
     if strand == "-":
@@ -21,8 +34,8 @@ def bed12line(chr, start, stop, strand, shift):
     score = 0
     color = "255,0,0"
     diff = stop - start - 1
-    return ("{chr}\t{start}\t{stop}\t{shift}\t{score}\t{arrow}\t"
-            "{start}\t{stop}\t{color}\t2\t1,1\t0,{diff}").format(**locals())
+    return [chrom, start, stop, shift, score, arrow,
+                start, stop, color, 2, "1,1", 0, diff]
 
 def sites_to_dict(bed):
     d = {}
@@ -30,20 +43,41 @@ def sites_to_dict(bed):
         d[l.name] = l
     return d
 
+def shifts_to_dict(cols, fname):
+    d = {}
+    for c in cols:
+        d[c] = {}
+        for l in reader(fname):
+            if not l[c] == "proximal" and not l[c] == "distal": continue
+            d[c][l['Sites']] = l[c]
+    return d
+
 def main(args):
-    sites = sites_to_dict(args.sites)
+    color = "0,0,255"
+    try:
+        strand = get_strand(args.shifts)
+    except StrandNotFound:
+        print >>sys.stderr, "\nStrand ('pos', 'neg') must be in file name.\n"
+        sys.exit(1)
+    refsites = sites_to_dict(args.sites)
     cols = reader(args.shifts, header=False).next()
-    shift_col = cols[-1]
-    if args.trackline:
-        print ('track type=bed name="{name}" description="{name}" '
-                'color={color}').format(name=args.name, color=args.color)
-    for l in reader(args.shifts):
-        a, b = l['Sites'].split(",")
-        a = sites[a]
-        b = sites[b]
-        shift = l[shift_col]
-        if shift != "proximal" and shift != "distal": continue
-        print bed12line(a.chrom, a.start, b.stop, a.strand, shift)
+    comparisons = cols[2:]
+    shifts = shifts_to_dict(comparisons, args.shifts)
+    for comparison, all_sites in shifts.iteritems():
+        lines = []
+        for (sites, shift) in all_sites.iteritems():
+            a, b = sites.split(",")
+            a = refsites[a]
+            b = refsites[b]
+            lines.append(bed12line(a.chrom, a.start, b.stop, a.strand, shift))
+        lines = sorted(lines, key=operator.itemgetter(0, 1))
+        f = gzip.open("{comparison}.{strand}.track.bed.gz".format(**locals()), 'wb')
+        f.write(('track type=bed name="{comparison}"'
+                    ' description="{comparison} {strand}"'
+                    ' color={color}\n').format(**locals()))
+        for line in lines:
+            f.write("\t".join(map(str, line)) + "\n")
+        f.close()
 
 if __name__ == '__main__':
     import argparse
@@ -51,13 +85,5 @@ if __name__ == '__main__':
             formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     p.add_argument("shifts", help="output of `classify_shifts.py`")
     p.add_argument("sites", help="output of `merge_sites.py`")
-    g = p.add_argument_group("trackline options")
-    g.add_argument("--trackline", action="store_true",
-            help="print UCSC trackline")
-    g.add_argument('--color', default="0,0,255", help="track color")
-    g.add_argument('--name', help="trackline name and description")
     args = p.parse_args()
-    if args.trackline and not args.name:
-        print >>sys.stderr, "\nMust supply --name with trackline.\n"
-        sys.exit(1)
     main(args)

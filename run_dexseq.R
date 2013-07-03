ee5a1b1b86a850f3cf4fdee90c09d7c867486068
brwnj <brwnjm@gmail.com>
2013-07-03 17:12:12 -0600

    correction for negative intercept

--- run_dexseq.R
+++ run_dexseq.R
@@ -30,7 +30,26 @@ cds <- read.HTSeqCounts(countfiles=c(afiles, bfiles), design=design)
 cds <- estimateSizeFactors(cds)
 cds <- estimateDispersions(cds, minCount=1, nCores=4, quiet=TRUE)
 cds <- fitDispersionFunction(cds)
+cds <- estimatelog2FoldChanges(cds, nCores=4)
 cds <- testForDEU(cds, nCores=4)
-cds <- estimatelog2FoldChanges(cds)
 res <- DEUresultTable(cds)
+
+# fix negative or zero intercept
+if(is.na(table(is.na(res$log2fold))["FALSE"]) && cds@dispFitCoefs <= 0){
+    # start over...
+    cds <- read.HTSeqCounts(countfiles=c(afiles, bfiles), design=design)
+    cds <- estimateSizeFactors(cds)
+    cds <- estimateDispersions(cds, minCount=1, nCores=4, quiet=TRUE)
+    cds <- fitDispersionFunction(cds)
+    
+    cds@dispFitCoefs[1] <- 0.001
+    fData(cds)$dispFitted <- cds@dispFitCoefs[1] + 
+        cds@dispFitCoefs[2] / colMeans(t(counts(cds)) / sizeFactors(cds) )
+    fData(cds)$dispersion <- pmin(pmax(fData(cds)$dispBeforeSharing, 
+                                       fData(cds)$dispFitted, na.rm = TRUE), 1e+08)
+    cds <- estimatelog2FoldChanges(cds, nCores=4)
+    cds <- testForDEU(cds, nCores=4)
+    res <- DEUresultTable(cds)
+}
+
 write.table(res, file=output, sep="\t", col.names=NA)

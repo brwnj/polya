cff761baf8b27a43c5cdd89165a8c3199f344611
brwnj <brwnjm@gmail.com>
2013-07-25 14:13:18 -0600

    start min_count at 10; step_size to find optimal count filter

--- run_dexseq.R
+++ run_dexseq.R
@@ -1,5 +1,5 @@
 # Usage:
-# Rscript run_dexseq.R a_names a_files b_names b_files result_file
+# Rscript run_dexseq.R a_names a_files b_names b_files result_file step_size=4
 # names and files are comma delimited lists
  
 # source("http://bioconductor.org/biocLite.R")
@@ -13,8 +13,9 @@ groupa_files <- args[2]
 groupb_names <- args[3]
 groupb_files <- args[4]
 output <- args[5]
+step_size <- ifelse(is.na(args[6]), 4, as.integer(args[6]))
 
-min_count <- 1
+min_count <- 10
 
 groupa_name <- gsub(",", "", groupa_names, fixed=TRUE)
 groupb_name <- gsub(",", "", groupb_names, fixed=TRUE)
@@ -27,29 +28,42 @@ lb <- length(bfiles)
 stopifnot(la == lb)
 
 design <- data.frame(row.names=(c(anames, bnames)),
-            condition=c(rep(groupa_name, la), rep(groupb_name, lb)))
-counts <- read.HTSeqCounts(countfiles=c(afiles, bfiles), design=design)
-cds <- estimateSizeFactors(counts)
-cds <- estimateDispersions(cds, minCount=min_count, nCores=4, quiet=TRUE)
-cds <- fitDispersionFunction(cds)
-cds <- estimatelog2FoldChanges(cds, nCores=4)
-cds <- testForDEU(cds, nCores=4)
-res <- DEUresultTable(cds)
-
-# fix negative or zero intercept
-if(is.na(table(is.na(res$log2fold))["FALSE"]) && cds@dispFitCoefs <= 0){
-    # start over...
+                     condition=c(rep(groupa_name, la), rep(groupb_name, lb)))
+counts <- read.HTSeqCounts(countfiles=c(afiles, bfiles),
+                           design=design)
+previous <- NA
+repeat{
     cds <- estimateSizeFactors(counts)
     cds <- estimateDispersions(cds, minCount=min_count, nCores=4, quiet=TRUE)
     cds <- fitDispersionFunction(cds)
-    
-    cds@dispFitCoefs[1] <- 0.001
-    fData(cds)$dispFitted <- cds@dispFitCoefs[1] + 
-        cds@dispFitCoefs[2] / colMeans(t(counts(cds)) / sizeFactors(cds) )
-    fData(cds)$dispersion <- pmin(pmax(fData(cds)$dispBeforeSharing, 
-                                       fData(cds)$dispFitted, na.rm = TRUE), 1e+08)
     cds <- estimatelog2FoldChanges(cds, nCores=4)
     cds <- testForDEU(cds, nCores=4)
     res <- DEUresultTable(cds)
+    
+    # fix negative or zero intercept
+    if(is.na(table(is.na(res$log2fold))["FALSE"]) && cds@dispFitCoefs[1] <= 0){
+        cds <- estimateSizeFactors(counts)
+        cds <- estimateDispersions(cds, minCount=min_count, nCores=4, quiet=TRUE)
+        cds <- fitDispersionFunction(cds)
+    
+        cds@dispFitCoefs[1] <- 0.001
+        fData(cds)$dispFitted <- cds@dispFitCoefs[1] + 
+            cds@dispFitCoefs[2] / colMeans(t(counts(cds)) / sizeFactors(cds) )
+        fData(cds)$dispersion <- pmin(pmax(fData(cds)$dispBeforeSharing, 
+                                           fData(cds)$dispFitted, na.rm = TRUE), 1e+08)
+        # drop log2fold from cds
+        cds <- estimatelog2FoldChanges(cds, nCores=4)
+        cds <- testForDEU(cds, nCores=4)
+        res <- DEUresultTable(cds)
+    }
+    
+    # number of significant sites
+    current <- as.integer(table(res$padjust < 0.05)[2])
+    if(!is.na(previous) && current < previous){
+        break
+    }
+    previous <- current
+    min_count <- min_count + step_size
 }
+
 write.table(res, file=output, sep="\t", col.names=NA)

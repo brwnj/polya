49882d3330c23c528938f896dbaea4fa7187a06b
brwnj <brwnjm@gmail.com>
2013-08-29 15:53:55 -0600

    better sample name handling; full gzip support; maintain header option

--- misc/filter_primary_site_interactions.py
+++ misc/filter_primary_site_interactions.py
@@ -4,20 +4,30 @@
 Filter Fisher test results to show only those involving a primary site from
 either sample in the comparison.
 """
+import os
 import re
 import pandas
+import signal
 import argparse
 from toolshed import header, reader
 
+signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+
 def get_ab(fname):
-    a, b = re.findall("(\w{2}\d+)", fname)
+    fname = os.path.basename(fname)
+    a, b = re.findall("(\w{2}\d+)[_.]", fname)
     strand = "pos" if "pos" in fname else "neg"
     return a + "." + strand, b + "." + strand
 
-def main(tests, xref):
-    df = pandas.read_table(xref, index_col=[0])
+def get_comp(fname):
+    return "gzip" if fname.endswith(".gz") else None
+
+def main(tests, xref, head):
+    df = pandas.read_table(xref, index_col=[0], compression=get_comp(xref))
     a, b = get_ab(tests)
     out_order = header(tests)
+    if head:
+        print "\t".join(out_order)
     for l in reader(tests):
         try:
             if df[a][l['SiteA']] or df[b][l['SiteA']] or df[a][l['SiteB']] or df[b][l['SiteB']]:
@@ -31,5 +41,6 @@ if __name__ == '__main__':
             formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     p.add_argument('test_results', help="fisher test result text file")
     p.add_argument('primary_site_table', help="primary site annotation table")
+    p.add_argument('--header', dest="head", action='store_true', help="maintain the file header")
     args = p.parse_args()
-    main(args.test_results, args.primary_site_table)
+    main(args.test_results, args.primary_site_table, args.head)

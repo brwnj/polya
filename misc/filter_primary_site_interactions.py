66bb3a70c5704ae8074da0ede267acc977cdb5f1
brwnj <brwnjm@gmail.com>
2013-12-10 16:24:59 -0700

    accounting for different sample name variations

--- misc/filter_primary_site_interactions.py
+++ misc/filter_primary_site_interactions.py
@@ -2,20 +2,28 @@
 # encoding: utf-8
 """
 Filter Fisher test results to show only those involving a primary site from
-either sample in the comparison.
+either sample in the comparison. Expects file names coming in to be
+sample1_to_sample2.strand.etc.txt.
 """
 import os
-import re
 import pandas
 import signal
-import argparse
 from toolshed import header, reader
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 signal.signal(signal.SIGPIPE, signal.SIG_DFL)
 
 def get_ab(fname):
+    """
+    >>> get_ab("NBT_to_TS-ERP.neg.fisher.txt.gz")
+    ('NBT.neg', 'TS-ERP.neg')
+    >>> get_ab("PK95_to_PK105.pos.fisher.txt.gz")
+    ('PK95.pos', 'PK105.pos')
+    """
     fname = os.path.basename(fname)
-    a, b = re.findall("(\w{2}\d+)[_.]", fname)
+    parts = fname.split(".", 1)[0].split("_")
+    a = parts[0]
+    b = parts[2]
     strand = "pos" if "pos" in fname else "neg"
     return a + "." + strand, b + "." + strand
 
@@ -37,8 +45,7 @@ def main(tests, xref, head):
             pass
 
 if __name__ == '__main__':
-    p = argparse.ArgumentParser(description=__doc__,
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    p = ArgumentParser(description=__doc__, formatter_class=ArgumentDefaultsHelpFormatter)
     p.add_argument('test_results', help="fisher test result text file")
     p.add_argument('primary_site_table', help="primary site annotation table")
     p.add_argument('--header', dest="head", action='store_true', help="maintain the file header")
